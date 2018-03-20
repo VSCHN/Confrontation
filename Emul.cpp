f99ea77096a317547695381125a5e813908a7210
VSCHN <visochan123@yandex.ru>
2018-03-20 18:43:23 +0700

    1st commit
    test commiting

--- Emul.cpp
+++ Emul.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-//#include <cstdlib>
+#include <cstdlib>
 
 using namespace std;
 int R=0,Q=0,W=0,Z=0,N=1,res=0;
@@ -14,17 +14,17 @@ int main(){
     int res1[K], res2[K];
     for (N; N<=Z;N++){
         system("C:\\Users\\VISci-Portable\\Documents\\GitHub\\Confrontation\\prog1.exe"); //Запуск игрока 1
-        ifstream fin("output1.txt"); //Чтение результатов игрока 1
+        ifstream fin1 ("output1.txt"); //Чтение результатов игрока 1
         for (int i=0;i<K;i++) {
-                fin>>res1[i];
-            }
-        fin.close();
+            fin1 >>res1[i];
+        }
+        fin1.close();
         system("C:\\Users\\VISci-Portable\\Documents\\GitHub\\Confrontation\\prog2.exe"); //Запуск игрока 2
-        ifstream fin("output2.txt"); //Чтение результатов игрока 2
+        ifstream fin2 ("output2.txt"); //Чтение результатов игрока 2
         for (int i=0;i<K;i++) {      //
-                fin>>res2[i];        //
-            }                        //
-        fin.close();
+            fin2 >>res2[i];        //
+        }                        //
+        fin2.close();
         int win1tmp = 0;
         int win2tmp = 0;
         for (int i = 0; i<K;i++){
@@ -33,17 +33,25 @@ int main(){
                 win2tmp++;
                 win1++;
                 win2++;
-            }else{
+            }
+            else{
                 if(res1[i]<res2[i]){
                     win2=win2+Q;
                     win2tmp=win2tmp+Q;
-                }else{
+                }
+                else{
                     win1=win1+Q;
                     win1tmp=win1tmp+Q;
                 }
             }
         }
-
+        ifstream fin("input.txt");
+        fin >> K >> R >> Q >> Z;
+        fin.close();
     }
-
+    ofstream dataout("Protokol.txt");          //Определение условий игры
+    for int (dt=1; dt<=Z;dt++){
+        dataout >>dt>>" ">>res1>>" ">>win1>>" ">>res2>>" ">> win2;
+    }
+    dataout.close();
 }
